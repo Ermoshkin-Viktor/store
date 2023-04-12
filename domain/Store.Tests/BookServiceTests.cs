83279c219f348e5b935f459f6941bccc36e16e02
Ermoshkin-Viktor <15erm@mail.ru>
2023-04-12 19:16:40 +0300

    Изменения теста

--- domain/Store.Tests/BookServiceTests.cs
+++ domain/Store.Tests/BookServiceTests.cs
@@ -11,27 +11,9 @@ namespace Store.Tests
     public class BookServiceTests
     {
         //Поиск по ISBN
-        //[Fact]
-        //public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
-        //{
-        //    //заглушка для BookRepository
-        //    var bookRepositoryStub = new Mock<IBookRepository>();
-        //    //если будет вызываться метод GetAllByIsbn с любым строковым параметром
-        //    //возратим массив типа Book c Id=1 и пустыми строками вместо названий
-        //    //It.IsAny<string>() - это экспрешенс выражение(деревья выражения), т.е. код не генерируется
-        //    //а выводится тип, т.е. дерево и потом сохраняется.
-        //    //Т.е. мы перехватили вызов 2-х методов
-        //    bookRepositoryStub.Setup(x => x.GetAllByIsbn(It.IsAny<string>()))
-        //                       .Returns(new[] { new Book(1, "", "", "") });
-        //    //возратим массив типа Book c Id=2 и пустыми строками вместо названий
-        //    bookRepositoryStub.Setup(x => x.GetAllByTitleOrAuthor(It.IsAny<string>()))
-        //                       .Returns(new[] { new Book(2, "", "", "") });
-        //    //В объект BookService в качестве параметра передаем bookRepository
-        //    //т.е. свойство Object нашей заглушки. Он выглядит точно так как 
-        //    //BookRepository . Но вместо него выведет нужный нам массив
-        //    var bookService = new BookService(bookRepositoryStub.Object);
+       
 
-        [Fact]
+            [Fact]
         public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
         {
             //заглушка для BookRepository
@@ -52,17 +34,17 @@ namespace Store.Tests
             var bookService = new BookService(bookRepositoryStub.Object);
 
 
-            //    var validIsbn = "ISBN 12345-67890";
+            var validIsbn = "ISBN 12345-67890";
 
-            //    var actual = bookService.GetAllByQuery(validIsbn);
-            //    //Проверяем значение Id (1 или 2)
-            //    //Equal(ожидаемое значение, передаваемое значение)
-            //    Assert.Collection(actual, book => Assert.Equal(1, book.Id));
-            //}
+            var actual = bookService.GetAllByQuery(validIsbn);
+            //Проверяем значение Id (1 или 2)
+            //Equal(ожидаемое значение, передаваемое значение)
+            Assert.Collection(actual, book => Assert.Equal(1, book.Id));
+        }
 
-            //Поиск по автору
-            [Fact]
-              void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
+        //Поиск по автору
+        [Fact]
+            public  void GetAllByQuery_WithAuthor_CallsGetAllByTitleOrAuthor()
             {
                 //заглушка для BookRepository
                 var bookRepositoryStub = new Mock<IBookRepository>();
@@ -96,7 +78,7 @@ namespace Store.Tests
 
             //Второй вариант тестов 
             [Fact]
-             void GetAllByQuery_WithIsbn_CallsGetAllByIsbn()
+             public void GetAllByQuery_WithIsbn_CallsGetAllByIsbn1()
             {
                 const int idOfIsbnSearch = 1;
                 const int idOfAuthorSearch = 2;
@@ -147,4 +129,4 @@ namespace Store.Tests
             }
         }
     }
-} 
+
