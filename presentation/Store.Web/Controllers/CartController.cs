104664d87070d0f637dadbe9650df0d3caa84b2b
Ermoshkin-Viktor <15erm@mail.ru>
2023-04-14 10:03:16 +0300

    комментарии к действию  Add в CartController

--- presentation/Store.Web/Controllers/CartController.cs
+++ presentation/Store.Web/Controllers/CartController.cs
@@ -12,20 +12,24 @@ namespace Store.Web.Controllers
         {
             this.bookRepository = bookRepository;
         }
-
+        //добавляем книгу в корзину
         public IActionResult Add(int id)
         {
             var book = bookRepository.GetById(id);
             Cart cart;
+            //Если корзины с таким id нет
             if (!HttpContext.Session.TryGetCart(out cart))
-                cart = new Cart();
-
+                cart = new Cart();//создаем новую пустую корзину
+            //Если книга с таким id уже есть в корзине
             if (cart.Items.ContainsKey(id))
+                //то увеличиваем к-во на единицу
                 cart.Items[id]++;
             else
+                //иначе к-во книг = 1
                 cart.Items[id] = 1;
+            //и стоимость корзины увеличиваем на цену книги
             cart.Amount += book.Price;
-
+            //сохраняем сессию
             HttpContext.Session.Set(cart);
 
             return RedirectToAction("Index", "Book", new { id });
