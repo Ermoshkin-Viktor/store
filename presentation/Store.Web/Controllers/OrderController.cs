a484ee9d61e3f29bee17dd3b3ba9b1d45ff43d0e
Ermoshkin-Viktor <15erm@mail.ru>
2023-05-07 14:08:38 +0300

    Подтверждать номер телефона

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -1,10 +1,14 @@
 ﻿
 
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Store.Messages;
 using Store.Web.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Store.Web.Controllers
 {
@@ -12,13 +16,17 @@ namespace Store.Web.Controllers
     {
         private readonly IBookRepository bookRepository;
         private readonly IOrderRepository orderRepository;
+        private readonly INotificationService notificationService;
 
-        public OrderController(IBookRepository bookRepository, IOrderRepository orderRepository)
+        public OrderController(IBookRepository bookRepository, IOrderRepository orderRepository,
+                         INotificationService notificationService)
         {
             this.bookRepository = bookRepository;
             this.orderRepository = orderRepository;
+            this.notificationService = notificationService;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             //если в корзине есть книги
@@ -42,20 +50,21 @@ namespace Store.Web.Controllers
                              select new OrderItemModel
                              {
                                  BookId = book.Id,
-                                 Title= book.Title,
-                                 Author= book.Author,
-                                 Price= item.Price,
-                                 Count= item.Count
+                                 Title = book.Title,
+                                 Author = book.Author,
+                                 Price = item.Price,
+                                 Count = item.Count
                              };
             return new OrderModel
             {
-                Id = order.Id,             
+                Id = order.Id,
                 Items = itemModels.ToArray(),
-                TotalCount= order.TotalCount,
-                TotalPrice= order.TotalPrice,
+                TotalCount = order.TotalCount,
+                TotalPrice = order.TotalPrice,
             };
         }
 
+        [HttpPost]
         public IActionResult AddItem(int bookId, int count = 1)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
@@ -83,16 +92,6 @@ namespace Store.Web.Controllers
             return RedirectToAction("Index", "Order");
         }
 
-        private void SaveOrderAndCart(Order order, Cart cart)
-        {
-            orderRepository.Update(order);
-            //обновляем к-во и цену
-            cart.TotalCount = order.TotalCount;
-            cart.TotalPrice = order.TotalPrice;
-            //сохраняем сессию
-            HttpContext.Session.Set(cart);
-        }
-
         private (Order order, Cart cart) GetOrCreateOrderAndCart()
         {
             Order order;
@@ -111,6 +110,17 @@ namespace Store.Web.Controllers
             return (order, cart);
         }
 
+        private void SaveOrderAndCart(Order order, Cart cart)
+        {
+            orderRepository.Update(order);
+            //обновляем к-во и цену
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            //сохраняем сессию
+            HttpContext.Session.Set(cart);
+        }
+
+        [HttpPost]
         public IActionResult RemoveItem(int bookid)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
@@ -118,8 +128,83 @@ namespace Store.Web.Controllers
             order.RemoveItem(bookid);
 
             SaveOrderAndCart(order, cart);
-            
+
             return RedirectToAction("Index", "Order");
+        }
+
+        [HttpPost]
+        public IActionResult SendConfirmationCode(int id, string cellPhone)
+        {
+            var order = orderRepository.GetById(id);
+            var model = Map(order);
+
+            if (!IsValidCellPhone(cellPhone))
+            {
+                model.Errors["cellPhone"] = "Номер телефона не соответствует формату +79876543210";
+                return View("Index", model);
+            }
+
+            int code = 1111;//random.Next(1000, 10000)
+            HttpContext.Session.SetInt32(cellPhone, code);
+            notificationService.SendConfirmationCode(cellPhone, code);
+
+            return View("Confirmation", 
+                         new ConfirmationModel 
+                         { 
+                             OrderId = id, 
+                             CellPhone = cellPhone 
+                         });
+        }
+
+        private bool IsValidCellPhone(string cellPhone)
+        {
+            if (cellPhone == null)
+            {
+                return false;
+            }
+            cellPhone = cellPhone.Replace(" ", "")
+                                .Replace("-", "");
+
+            return Regex.IsMatch(cellPhone, @"^\+?\d{11}$");
+        }
+
+        [HttpPost]
+        public IActionResult StartDelivery(int id, string cellPhone, int code)
+        {
+            //загружаем код из сессии с предыдущего шага
+            int? storedCode = HttpContext.Session.GetInt32(cellPhone); 
+            //если там ничего не хранилось
+            if(storedCode == null)
+            {
+                return View("Confirmation",
+                        new ConfirmationModel
+                        {
+                            OrderId = id,
+                            CellPhone = cellPhone,
+                            Errors = new Dictionary<string, string> 
+                            {
+                                { "code", "Пустой код, повторите отправку "}
+                            },
+                        }) ; 
+            }
+
+            if(storedCode != code)
+            {
+                return View("Confirmation",
+                        new ConfirmationModel
+                        {
+                            OrderId = id,
+                            CellPhone = cellPhone,
+                            Errors = new Dictionary<string, string>
+                            {
+                                { "code", "Отличается от отправленного"}
+                            },
+                        });
+            }
+
+            //
+            return View();
         }
     }
 }
+
